5898c6a3efb1e1916f99f7c6ce86c68a0003f8bc
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-31 11:07:42 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+// import React, { createContext } from 'react';
+// import './style.css';
+// import ComB from './Child.js';
+// const FirstNmae = createContext();
+// export default function App() {
+//   return (
+//     <>
+//       <FirstNmae.Provider value={'israrKhan'}>
+//         <ComB />
+//       </FirstNmae.Provider>
+//     </>
+//   );
+// }
+// export { FirstNmae };
+
 import React, { useState, createContext } from 'react';
-import './style.css';
-import Child from './Child';
+import ChildComponent from './Child';
 const GlobalStateContext = createContext();
-export default function App() {
+
+const App = ({ children }) => {
   const [state, setState] = useState({});
+
   return (
-    <GlobalStateContext.Provider value={'khan'}>
-      <Child />
+    <GlobalStateContext.Provider value={[state, setState]}>
+      {children}israr <ChildComponent/>
     </GlobalStateContext.Provider>
   );
-}
-export { GlobalStateContext };
+};
+export default App;
+export {GlobalStateContext};
--- src/Child.js
+++ src/Child.js
@@ -1,7 +1,23 @@
-import React, { useContext } from 'react';
-import GlobalStateContext from './App';
-const Child = () => {
-  const myvalue = useContext(GlobalStateContext);
-  return <div>what {myvalue}</div>;
+// import React, { useContext, useState } from 'react';
+// import { FirstNmae } from './App';
+// const Child = () => {
+//   const myvalue = useContext(FirstNmae);
+//   return <div>what {myvalue}</div>;
+// };
+// export default Child;
+
+import React, { useContext, createContext } from 'react';
+import { GlobalStateContext } from './App';
+const ChildComponent = () => {
+  const [state, setState] = useContext(GlobalStateContext);
+  const handleClick = () => {
+    setState((prevState) => ({ ...prevState, message: 'Hello, world!' }));
+  };
+  return (
+    <div>
+      <p>{state.message}</p>
+      <button onClick={handleClick}>Update State</button>
+    </div>
+  );
 };
-export default Child;
+export default ChildComponent;
